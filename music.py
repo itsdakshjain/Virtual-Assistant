12afbfd413b82378988fd67d85ba29cc1e203e66
itsdakshjain <dakshjain1265@gmail.com>
2024-09-07 00:51:44 +0530

    Create music.py

---
+++ music.py
@@ -0,0 +1,7 @@
+music_dict={'senorita' : "https://www.youtube.com/watch?v=Pkh8UtuejGw&pp=ygUOc29uZ3Mgc2Vub3JpdGE%3D&autoplay=1" ,
+             "lover" : "https://www.youtube.com/watch?v=mH_LFkWxpI0&pp=ygUFbG92ZXI%3D&autoplay=1",
+             "tum hi ho": "https://www.youtube.com/watch?v=IJq0yyWug1k&pp=ygUJdHVtIGhpIGhv&autoplay=1",
+    "jeene laga hoon": "https://www.youtube.com/watch?v=qpIdoaaPa6U&pp=ygUaamVuYSBsYWdhIGhvb24gYXRpZiBhc2xhbSA%3D&autoplay=1", 
+    "channa mereya": "https://www.youtube.com/watch?v=284Ov7ysmfA&pp=ygUMY2hubmEgbWVyZXlh&autoplay=1"
+             
+           }
